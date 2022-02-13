d42ac02e48c9d231d7d4798d5c37478ae8b7bd61
desertificacion-AI <99278856+desertificacion-AI@users.noreply.github.com>
2022-02-13 18:24:40 +0100

    New review

--- desertificacion_ai.py
+++ desertificacion_ai.py
@@ -3,67 +3,72 @@ from PIL import Image
 
 st.set_page_config(
     page_title='Aplicación megachula',
-    page_icon='⚡',
+    page_icon=':cactus:',
     layout='centered'
 )
 
 # Título
 # ======
 
-st.title('Mis pruebas con streamlit')
+st.title('Desertificación en Aragón')
 st.subheader('AI Saturdays - I Edición Zaragoza - CURSO BÁSICO DE IA')
-st.markdown("""
-#### Pruebas con `streamlit` para el proyecto **Desertificación Aragón**
-""")
+st.markdown('''### Descripción del problema''')
+st.markdown('''
+La desertificación se produce por la degradación de la tierra que se da en zonas secas: áridas, semiáridas y \
+subhúmedas secas. La actividad humana y las variaciones climáticas están entre las causas de esta degradación del \
+suelo, que impacta en los ecosistemas y en los recursos y modos de vida de los habitantes de las zonas afectadas. \
+Estas zonas secas son aquellas con un índice de aridez (IA=P/ETP) inferior a 0,65.
+España es uno de los territorios que sufre esta problemática.
+
+En Aragón el 75% del territorio está en riesgo de desertización. El cambio climático y la subida de las temperaturas \
+amenazan las tierras semiáridas que predominan en el Valle del Ebro, las zonas secas antes mencionadas son justamente \
+las que se dan en el valle del Ebro, desde la ciudad de Huesca hacia el sur.
+
+Para poder identificar estas zonas se propone un proceso que gestione el reconocimiento por imágenes de la \
+desertificación en Aragón con el objetivo de aplicar políticas más eficientes referentes a la reforestación de las \
+zonas afectadas, y nuevos tratamientos de cultivos.
+''')
+st.markdown('''### Descripción del problema''')
+st.markdown('''
+El objetivo de la aplicación en una primera instancia pasa por, dada una imagen de la zona recogida de satelite poder \
+comparar la evolucion en el tiempo con respecto a la desertificación *y en una segunda instancia poder \
+desarrollar un modelo que regrese la probabilidad de desertizacion en un area dada*.
+''')
 
 # Banda lateral
 # =============
 
 st.sidebar.header('Banda lateral')
-
-uploaded_img = st.sidebar.file_uploader('Arrastra aquí la imagen')
+st.text('Aquí se pueden poner algunos widgets')
 
 # Imagen
 # ======
 
 st.markdown('---')
-st.subheader('Leyendo la imagen directamente desde el archivo')
+st.subheader('Imagen geoespacial')
 
-code = """
-image_name = 'LC08_L1TP_199031_20220117_20220123_02_T1_ndvi_M_[-5.3173828125,39.72831341029745,3.8232421875000004,42.879989517714826].tiff'
-image = Image.open(image_name)
-st.image(image, width=800)
-"""
-st.code(code, language='python')
-
+# Imagen de prueba
 image_name = 'LC08_L1TP_199031_20220117_20220123_02_T1_ndvi_M_[-5.3173828125,39.72831341029745,3.8232421875000004,42.879989517714826].tiff'
 image = Image.open(image_name)
 st.image(image, width=800)
 
-st.markdown('---')
-st.subheader('Arrastrando la imagen en la banda lateral')
+# Librerías
+# =========
 
-code = """
-uploaded_img = st.sidebar.file_uploader('Arrastra aquí la imagen')
-try:
-    image_name = uploaded_img
-    image = Image.open(image_name)
-    st.image(image, width=800)
-except:
-    pass
-"""
-st.code(code, language='python')
-
-try:
-    image_name = uploaded_img
-    image = Image.open(image_name)
-    st.image(image, width=800)
-except:
-    pass
+with st.expander('Librerías'):
+    st.markdown("""
+        PIL
+        streamlit
+        ...
+    """)
 
 # Créditos
+# ========
+
 with st.expander('Créditos'):
     st.markdown("""
+        13/02/2022
+        
         * Pedro Biel
         * Yinet Castiblanco
         * Eva de Miguel
